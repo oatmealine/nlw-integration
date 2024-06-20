23e03223f458bbc86655bc958c2bf72f0a8357a7
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-06-21 00:28:17 +0300

    update tier colors

--- src/ListManager.cpp
+++ src/ListManager.cpp
@@ -128,7 +128,8 @@ cocos2d::ccColor3B ListManager::getTierColor(std::string tier) {
 	if (tier == "Extreme")     return cocos2d::ccColor3B(255, 12,  251);
 	if (tier == "Remorseless") return cocos2d::ccColor3B(157, 10,  250);
 	if (tier == "Relentless")  return cocos2d::ccColor3B(178, 135, 232);
-	if (tier == "Terrifying")  return cocos2d::ccColor3B(204, 204, 204);
+	if (tier == "Terrifying")  return cocos2d::ccColor3B(241, 158, 234);
+	if (tier == "Super Fucking Terrifying") return cocos2d::ccColor3B(0, 0, 0);
 
 	// pending
 	if (tier == "Low End")            return cocos2d::ccColor3B(0,   192, 237);
